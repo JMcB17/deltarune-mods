56fdd86755d0a7eb0cc637bac9077bde94958f01
JMcB <joel.mcbride1@live.com>
2021-11-04 23:35:12 +0000

    Fix repack script

--- sprites/new/_repack.py
+++ sprites/new/_repack.py
@@ -9,26 +9,26 @@ IMAGE_FORMAT = '.png'
 def process_sprite(sprite: Path, repack_dir: Path, multi: bool = False):
     if multi:
         dir_name = sprite.parent.stem
-        new_sprite_name = dir_name + sprite.stem
+        new_sprite_name = dir_name + '_' + sprite.stem
     else:
-        new_sprite_name = sprite.stem + '0'
-    
+        new_sprite_name = sprite.stem + '_0'
+
+    print(new_sprite_name)
     new_sprite_path = repack_dir.joinpath(new_sprite_name).with_suffix(IMAGE_FORMAT)
 
     shutil.copy(sprite, new_sprite_path)
 
 
-def process_dir(directory: Path, repack_dir: Path):
+def process_dir(directory: Path, repack_dir: Path, level: int = 0):
     dir_name = directory.stem
 
     for sprite in directory.iterdir():
         if sprite.is_dir():
-            process_dir(sprite, repack_dir)
+            if not sprite.name == REPACK_DIR_NAME:
+                process_dir(sprite, repack_dir, level=level + 1)
         
-        if not sprite.suffix == IMAGE_FORMAT:
-            continue
-
-        process_sprite(sprite, repack_dir, multi=True)
+        if sprite.suffix == IMAGE_FORMAT:
+            process_sprite(sprite, repack_dir, multi=(level != 0))
 
 
 def main():
